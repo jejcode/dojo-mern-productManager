4993af89159f24f93b29ecb7c7416bb05d82db35
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-06-09 11:01:40 -0600

    added delete function to ProductDetail

--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -1,11 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import {useParams} from 'react-router-dom'
+import {useParams, useNavigate} from 'react-router-dom'
 
 const ProductDetail = (props) => {
     const [product, setProduct] = useState({})
     const {id} = useParams()
+    const navigate = useNavigate()
 
+    const deleteProduct = (productId) => {
+        axios.delete(`http://localhost:8000/api/product/${productId}`)
+            .then(res => navigate('/products'))
+            .catch(err => console.log(err))
+    }
     useEffect(() => {
         axios.get(`http://localhost:8000/api/product/${id}`)
             .then( res => {
@@ -20,6 +26,7 @@ const ProductDetail = (props) => {
                 <h3>{product.title}</h3>
                 <p>Price: ${product.price}</p>
                 <p>Description: {product.description}</p>
+                <button className="btn btn-sm btn-outline-danger mx-1" onClick={(e) => {deleteProduct(product._id)}}>Delete</button>
             </div>
         </div>
     )
