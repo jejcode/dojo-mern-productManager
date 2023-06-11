d23213553e07fca6381bb06e16d208c3a207d89f
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-06-11 11:09:05 -0600

    created DeleteButton to reuse

---
+++ client/src/components/DeleteButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { deleteProductById } from "../services/product-service";
+
+const DeleteButton = (props) => {
+    const {productId, successCallback} = props
+    const deleteProduct = () => {
+        deleteProductById(productId)
+        successCallback(productId)
+    }
+
+    return (
+        <button className="btn btn-outline-danger btn-sm" onClick={deleteProduct}>Delete</button>
+    )
+}
+
+export default DeleteButton
